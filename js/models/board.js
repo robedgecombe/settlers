59de24836217a07d054333f7727b784e9754ac28
Anthony <anthony@anthonymace.com>
2014-12-13 14:28:30 +1300

    refactored hexagon model

--- js/models/board.js
+++ js/models/board.js
@@ -10,25 +10,35 @@ Board.prototype = {
 	},
 	makeHexagon: function(x, y){
 		var c = 90;
-		  var b = Math.sqrt(c*c - (c/2)*(c/2));
-			var coords = [x - b, y + c/2, x, y+ c, x + b, y + c/2, x + b, y - c/2, x, y-c, x - b, y - c/2];
-			var x1 = x - b;
-			var y1 = y + c/2;
-			var x2 = x;
-			var y2 = y+ c;
-			var x3 = x + b;
-			var y3 = y + c/2;
-			var x4 = x + b;
-			var y4 = y - c/2;
-			var x5 = x;
-			var y5 = y-c;
-			var x6 = x - b;
-			var y6 = y - c/2;
-			var hexagon = two.makePolygon(x1, y1, x2, y2, x3, y3, x4, y4, x5, y5, x6, y6, true, false)
-			hexagon.fill = "#FFF";
-			hexagon.stroke = "#1c75bc";
-			hexagon.linewidth = 30;
-			return hexagon
+	  var b = Math.sqrt(c*c - (c/2)*(c/2));
+		var coords = {
+			topLeft: 		 {x: x - b, y: y + c/2},
+			top: 		 		 {x: x, 		y: y + c},
+			topRight:    {x: x + b, y: y + c/2},
+			bottomRight: {x: x + b, y: y - c/2},
+			bottom: 		 {x: x, 		y: y - c},
+			bottomLeft:  {x: x - b, y: y - c/2}
+		};
+		return coords
+	},
+	makeHexagonPoly: function(coords) {
+		var x1 = coords.topLeft.x
+		var y1 = coords.topLeft.y
+		var x2 = coords.top.x
+		var y2 = coords.top.y
+		var x3 = coords.topRight.x
+		var y3 = coords.topRight.y
+		var x4 = coords.bottomRight.x
+		var y4 = coords.bottomRight.y
+		var x5 = coords.bottom.x
+		var y5 = coords.bottom.y
+		var x6 = coords.bottomLeft.x
+		var y6 = coords.bottomLeft.y
+		var hexagon = two.makePolygon(x1, y1, x2, y2, x3, y3, x4, y4, x5, y5, x6, y6, true, false)
+		hexagon.fill = "#FFF";
+		hexagon.stroke = "#1c75bc";
+		hexagon.linewidth = 30;
+		return hexagon
 	},
 	findHexagonCentrePoint: function(serverX, serverY, canvasCentreX, canvasCentreY) {
 			var canvasXTemp;
@@ -44,7 +54,8 @@ Board.prototype = {
 	makeCanvasHexagon: function(serverX, serverY) {
 		var hexagonCentre = this.findHexagonCentrePoint(serverX, serverY, 500, 500);
 		var hexagon = this.makeHexagon(hexagonCentre[0], hexagonCentre[1])
-		return hexagon
+		var hexagonPoly = this.makeHexagonPoly(hexagon)
+		return hexagonPoly
 	},
 	makeHexagons: function(){
 		var thisBoard = this
